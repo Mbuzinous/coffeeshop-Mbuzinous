62044fabed3cb2c172a88dfcc4c720ea14cacb1f
Kevin <68694478+Mbuzinous@users.noreply.github.com>
2025-02-20 15:31:58 +0100

    Test working

---
+++ CoffeeShopConsoleAppNet60Tests/Coffees/CoffeeTests.cs
@@ -0,0 +1,41 @@
+﻿using CoffeeShopConsoleAppNet60.Coffees.BlackCoffees;
+
+namespace CoffeeShopConsoleAppNet60.Tests
+{
+    [TestClass()]
+    public class CoffeeTests
+    {
+        [DataTestMethod]
+        [DataRow(1, 19)]
+        [DataRow(2, 18)]
+        [DataRow(3, 17)]
+        [DataRow(4, 16)]
+        public void Test_Price_Method_Americanos(int discount, int expected)
+        {
+            //Arrange
+            Coffee americano = new Americano();
+
+            //Act
+            double result = americano.Price(discount);
+
+            //Assert
+            Assert.AreEqual(expected, result);
+        }
+        [DataTestMethod]
+        [DataRow(1, 15)]
+        [DataRow(2, 14)]
+        [DataRow(3, 13)]
+        [DataRow(4, 12)]
+        public void Test_Not_Price_Method_Americano(int discount, int expected)
+        {
+            //Arrange
+            Coffee americano = new Americano();
+
+            //Act
+            double result = americano.Price(discount);
+
+            //Assert
+            Assert.AreNotEqual(expected, result);
+        }
+    }
+}
